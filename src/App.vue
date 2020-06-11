28c4b24f7dbe15c8f2f2dc24993e7d7c0d7e9972
EasonO <1424102009@qq.com>
2020-06-11 16:45:35 +0800

    scass\

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,25 @@
 
 <script>
 export default {
-  name: 'app',
-  components: {
-    
-  },
   data(){
     return {
-      
+      res:{}
     }
   },
   mounted(){
-    
-  },
-  methods:{
-     
+    // 本地加载请求静态json文件的形式
+    // this.axios.get('/mock/user/login.json').then((res)=>{
+    //   this.res = res;
+    // })
+    // 通过easy-mock平台实现数据mock
+    // this.axios.get('/user/login').then((res)=>{
+    //   this.res = res;
+    // })
+    // 本地集成mockjs实现数据mock
+    this.axios.get('/user/login').then((res)=>{
+      this.res = res;
+    })
+
   }
-    
 }
 </script>
-
-<style lang="scss">
-
-</style>
