94b527306c3385710134a8ee5e61c6f94bd38108
EasonO <1424102009@qq.com>
2020-06-13 01:12:50 +0800

    完成footer部分

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="javascript:;">河畔一角主页</a><span>|</span>
+            <a href="javascript:;">Vue全栈课程</a><span>|</span>
+            <a href="javascript:;">React全家桶课程</a><span>|</span>
+            <a href="javascript:;">微信支付专项课程（H5+小程序+Node+Mongo）</a>
+        </div>
+        <div class="copyright">Copyright ©2019 <span class="domain">mi.futurefe.com</span> All Rights Reserved.</div>
     </div>
 </template>
 
@@ -9,3 +19,41 @@ export default {
     name:'nav-footer'
 }
 </script>
+
+<style lang="scss">
+@import "./../assets/scss/config.scss";
+.footer{
+    height: 234px;
+    border-top: 4px solid $colorA;
+    background-color: $colorB;
+    color: $colorD;
+    font-size: $fontI;
+    text-align: center;
+    .footer-logo{
+        margin-top: 46px;
+        margin-bottom: 31px;
+        img{
+            width: 53px;
+            height: 36px;
+            margin-bottom: 13px;
+        }
+    }
+    .footer-link{
+        a{
+            color: $colorD;
+            display: inline-block;
+        }
+        span{
+            margin: 0 10px;
+        }
+        margin-bottom: 13px;
+    }
+    .copyright{
+        .domain{
+            color: $colorA;
+        }
+    }
+}
+
+
+</style>
